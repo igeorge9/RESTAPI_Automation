05540fdc5c7004e3ae4601e062ae6057e17559b1
igeorge9 <irenegeorge9999@gmail.com>
2021-08-28 22:34:40 +0530

    new files added

--- src/com/Cucumber/Options/TestRunner.java
+++ src/com/Cucumber/Options/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/com/features/placeValidations.feature",
-plugin ={"json:target/jsonReports/cucumber-report.json","html:target/cucumber-html-report"},
+plugin={"pretty", "html:cucumberReport/cucumber-html-report","json:cucumberReport/cucumber-report.json"},
 glue={"cucumber/steps"},tags= ("@DeletePlace"))
 
 public class TestRunner {
